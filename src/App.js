757fe2c316fa7d5e7330b59169a1c53b18d1c50d
Daurelian <90376706+Daurelian@users.noreply.github.com>
2021-10-08 16:20:52 +0200

    router

--- src/App.js
+++ src/App.js
@@ -1,17 +1,36 @@
 import Characters from "./Characters";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 import React, {useState} from "react";
 import Navigation from "./Navigation";
+import SingleCharacter from "./singleCharacter";
+// import { Router } from "@mui/icons-material";
+
 
 function App() {
   const [page, setPage]= useState(1)
   const [maxPage, setMaxPage] =useState(0)
   return (
+    <Router>
     <div className='App'>
+    <Switch>
+       <Route path="/singleCharacter">
+      <SingleCharacter />
+      </Route>
+    <Route path="/">
       <Navigation page={page} setPage={setPage} maxPage={maxPage}/>
       <Characters page={page} setPage={setPage} setMaxPage={setMaxPage}/>
+      </Route>
+      </Switch>
     </div>
+     </Router>
   );
+  
 }
 
 export default App;
---
+++ src/singleCharacter.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function SingleCharacter(){
+
+return (
+    <h1>Ciao</h1>
+)
+
+
+
+
+
+
+}
